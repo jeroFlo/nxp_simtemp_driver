22a992ed615fefadf74fefdff31cbe699a560eee
Jenifer Romero <girasol_gril_flores@hotmail.com>
2025-10-14 19:07:51 -0600

    Adding option to make a clean from same script. Simplifying make command as well

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,11 +5,21 @@
  makefile_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )/../kernel
 
  if [ -n "$1" ]; then # check for a user input 
-    makefile_path=$1
-    echo "*** Overriding Makefile path by the user input ***"
+    if [[ "$1" -eq "-c" ]]; then # option to make a clean
+       cd $makefile_path
+       echo "*** Cleaning ***"
+       make clean -C $makefile_path 
+       exit 0
+    else # asumming a existing path to a makefile
+      makefile_path=$1
+      echo "*** Overriding Makefile path by the user input ***"
+    fi
+    
  fi
 
- make -C /lib/modules/`uname -r`/build M=$makefile_path modules
+ cd $makefile_path
+
+ make -C $makefile_path 
 
 
  # - user app
